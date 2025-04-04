259f34104c383273faf6f792986ef39f68b58cf1
Bogdan282 <bogdan673876@gmail.com>
2025-04-04 12:31:16 +0300

    Add functional. Converting mills to km

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Convertor Project");
         System.out.println("Version 1.0");
+
+        int km = 15;
+        int mills = 20;
+
+        System.out.println(mills + " миль це " + convertMillsToKm(mills) + " кілометрів.");
+    }
+    public static double convertMillsToKm(int mills) {
+        return mills * 1.60934;
     }
 }
